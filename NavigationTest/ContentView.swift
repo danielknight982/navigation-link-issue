5cf36e41358fb8bf6520a6a88dddf0e8dea426cc
Daniel S Knight <daniel.knight@gmail.com>
2020-07-23 16:15:36 -0500

    Initial Commit

---
+++ NavigationTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NavigationTest
+//
+//  Created by Daniel Knight on 7/23/20.
+//  Copyright © 2020 Daniel Knight. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
